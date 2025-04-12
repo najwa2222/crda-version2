1cdaf786b5403aa57f8d16d2c1e8e56c3eaa7060
najwa2222 <karrouchinajwa@gmail.com>
2025-04-12 19:43:59 +0100

    jest and supertest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,42 @@
+// @ts-nocheck
 const js = require('@eslint/js');
-const { node } = require('globals');
+const { node, jest } = require('globals');
+const eslintPluginJest = require('eslint-plugin-jest');
 
 module.exports = [
   js.configs.recommended,
   {
+    files: ['**/*.js'],
     languageOptions: {
       globals: {
         ...node,
         process: 'readonly',
-        console: 'readonly'
+        console: 'readonly',
       }
     },
     rules: {
-      "no-unused-vars": "warn",
-      "no-console": "off"
+      'no-unused-vars': 'warn',
+      'no-console': 'off'
     },
     ignores: [
-      "**/node_modules/",
-      "coverage/",
-      "public/",
-      "**/*.config.js"
-    ]
+      '**/node_modules/',
+      'coverage/',
+      'public/',
+      '**/*.config.js'
+    },
+  },
+  {
+    files: ['**/__tests__/**/*.js', '**/*.test.js'],
+    languageOptions: {
+      globals: {
+        ...jest
+      }
+    },
+    plugins: {
+      jest: eslintPluginJest
+    },
+    rules: {
+      // Jest-specific rules can go here
+    }
   }
 ];
